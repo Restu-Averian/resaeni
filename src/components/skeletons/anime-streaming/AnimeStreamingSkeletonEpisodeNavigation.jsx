4fbde6b8c9b8371f28b0b288da5a0ba294e6158e
Restu Averian <restuaverianputra123@gmail.com>
2026-08-17 23:00:50 +0700

    style: update layout for episode navigation skeleton and enhance styling for player skeleton

--- src/components/skeletons/anime-streaming/AnimeStreamingSkeletonEpisodeNavigation.jsx
+++ src/components/skeletons/anime-streaming/AnimeStreamingSkeletonEpisodeNavigation.jsx
@@ -8,16 +8,21 @@ function AnimeStreamingSkeletonEpisodeNavigation() {
       gap={{ base: "4", md: "6" }}
       py={{ base: "4", md: "5" }}
     >
-      <HStack justify="start">
+      <HStack justify="center">
         <Skeleton h={{ base: "24px", md: "28px" }} w="100px" />
       </HStack>
 
-      <Stack align="center" gap="1">
+      <Stack
+        align="center"
+        gap="0.5"
+        borderX={{ base: "0", md: "1px solid" }}
+        borderColor="border.default"
+      >
         <Skeleton h="36px" w="120px" />
         <Skeleton h="20px" w="150px" />
       </Stack>
 
-      <HStack justify="end">
+      <HStack justify="center">
         <Skeleton h={{ base: "24px", md: "28px" }} w="100px" />
       </HStack>
     </Grid>
